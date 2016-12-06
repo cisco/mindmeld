bd7598308eaf15564181b88dae2af6ec5adc831d
Karthik Raghunathan <karthik@Karthiks-MacBook-Pro.local>
2016-12-05 20:53:42 -0800

    Add RTD theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,7 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
+import sphinx_rtd_theme
 import sys
 sys.path.insert(0, os.path.abspath('../../mmworkbench'))
 
@@ -140,7 +141,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -149,7 +150,7 @@ todo_include_todos = True
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
