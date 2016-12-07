5b77e05a5fc543650ce0a1c2b2e3e95efb982e58
Dan Zezula <dan@expectlabs.com>
2016-12-07 10:24:38 -0800

    update script

--- docs/scripts/upload_to_s3.sh
+++ docs/scripts/upload_to_s3.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-tar czvf workbench-docs.tgz docs/
-sha=`echo ${CIRCLE_SHA1} | cut -c1-6`
-target=${CIRCLE_BRANCH}/workbench-docs.tar.gz
-bucket=mindmeld-docs
-aws s3 cp workbench-docs.tgz s3://${bucket}/${target}
+aws s3 sync docs/build/html s3://mindmeld-docs/${CIRCLE_BRANCH}/
