27a39b598f73a87f67cbd9aa3ff5ce6aaea91444
J.J. Jackson <jj@expectlabs.com>
2017-01-04 18:35:07 -0800

    Docs: Fix conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ import sys
 
 # Get the project root dir, which is the parent dir of this
 cwd = os.getcwd()
-project_root = os.path.dirname(cwd)
+project_root = os.path.dirname(os.path.dirname(cwd))
 
 # Insert the project root dir as the first element in the PYTHONPATH.
 # This lets us ensure that the source package is imported, and that its
