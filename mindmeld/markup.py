18bb9509a15c8d1f45cbeff84899791012ae2686
J.J. Jackson <jj@expectlabs.com>
2017-01-11 14:32:55 -0800

    First pass at markup implementation

--- mindmeld/markup.py
+++ mindmeld/markup.py
@@ -4,6 +4,13 @@ representing annotations of query text inline.
 """
 from __future__ import unicode_literals
 
+import re
+
+from mmworkbench.parser.data import Query, ParsedQuery, Entity
+
+ENTITY_PATTERN = re.compile('\{(.*?)\}')
+NUMERIC_PATTERN = re.compile('\[(.*?)\|num:(.*?)\]')
+
 
 def create_parsed_query_for_markup(markup, tokenizer, preprocessor, is_gold=False):
     """Creates a parsed query object from marked up query text. The
@@ -15,9 +22,14 @@ def create_parsed_query_for_markup(markup, tokenizer, preprocessor, is_gold=Fals
         is_gold (bool): True if the markup passed in is a reference, human-labeled example
 
     Returns:
-        ParsedQuery: a parsed
+        ParsedQuery: a parsed query
     """
-    pass
+
+    raw_text = _mark_down(markup)
+    query = Query(raw_text, tokenizer, preprocessor)
+    entities = _parse_entities(markup, query=query)
+
+    return ParsedQuery(query, entities=entities, is_gold=is_gold)
 
 
 def create_markup_for_parsed_query(parsed_query):
@@ -29,7 +41,9 @@ def create_markup_for_parsed_query(parsed_query):
     Returns:
         str: A marked up representation of the query
     """
-    pass
+    raw_text = parsed_query.query.raw_text
+    markup = _mark_up(raw_text, parsed_query.entities)
+    return markup
 
 
 def validate_markup(markup):
@@ -42,3 +56,101 @@ def validate_markup(markup):
         bool: True if the markup is valid
     """
     pass
+
+
+def _parse(markup):
+    """Given marked up text returns marked down text and a list of entities.
+
+    Args:
+        markup (str): marked up text
+
+    """
+
+    pass
+
+
+def _mark_down(markup):
+    return _mark_down_entities(_mark_down_numerics(markup))
+
+
+def _mark_up(raw_text, entities=[], numerics=[]):
+    # TODO: also mark up numerics
+    return _mark_up_entities(raw_text, entities)
+
+
+def _parse_entities(markup, query=None):
+    entities = []
+    for match in ENTITY_PATTERN.finditer(markup):
+        prefix = _mark_down(markup[:match.start()])
+        start = len(prefix)
+        clean_match_str = _mark_down_numerics(match.group(1))
+        components = clean_match_str.split('|')
+        if len(components) == 2:
+            entity_text, facet_name = components
+            role_name = None
+        else:
+            entity_text, facet_name, role_name = components
+
+        end = start - 1 + len(entity_text)
+        params = {'query': query, 'text_start': start, 'text_end': end, 'entity_type': facet_name,
+                  'role': role_name}
+        entities.append(Entity(**params))
+
+    return entities
+
+
+def _mark_down_entities(markup):
+    def markeddown(match):
+        entity = match.group(1)
+        r2 = '\|[^]]*$'
+        return re.sub(r2, '', entity)
+    return re.sub(ENTITY_PATTERN, markeddown, markup)
+
+
+def _mark_up_entities(query_str, entities):
+    # remove existing markup just in case
+    query_str = _mark_down_entities(query_str)
+
+    # make sure entities are sorted
+    sorted_entities = sorted(entities, key='start')
+    new_query = ''
+    cursor = 0
+
+    # add each entity
+    for entity in sorted_entities:
+        start = entity.text_start
+        end = entity.text_end + 1
+        new_query += query_str[cursor:start]
+        if entity.role is None:
+            new_query += "{{{}|{}}}".format(query_str[start:end], entity.type)
+        else:
+            new_query += "{{{}|{}|{}}}".format(query_str[start:end], entity.type, entity.role)
+        cursor = end
+    new_query += query_str[cursor:]
+    return new_query
+
+
+# TODO: figure out whether we need numerics
+
+def _parse_numerics(markup):
+    numerics = []
+    query_str = _mark_down(markup)
+    for match in NUMERIC_PATTERN.finditer(query_str):
+        entity_text = match.group(1)
+        prefix = _mark_down(query_str[:match.start()])
+
+        start = len(prefix)
+        end = start - 1 + len(entity_text)
+        numerics.append({'entity': entity_text, 'type': match.group(2),
+                         'start': start, 'end': end})
+    return numerics
+
+
+def _mark_down_numerics(markup):
+    # TODO: figure out whether we need this
+    return re.sub(NUMERIC_PATTERN, r'\1', markup)
+
+
+def _mark_up_numerics(markup, numerics):
+    # TODO: implement this if we need it
+    return markup
