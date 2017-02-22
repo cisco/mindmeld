5187baae73831eb3e4fd645a091548a0ecd950c2
J.J. Jackson <jj@expectlabs.com>
2017-02-22 10:31:39 -0800

    base_model -> base

--- mmworkbench/learners/ner/__init__.py
+++ mmworkbench/learners/ner/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
-from .base_model import FacetClassifier
+from .base import BaseEntityRecognizer
 
-__all__ = ['FacetClassifier']
+__all__ = ['BaseEntityRecognizer']
