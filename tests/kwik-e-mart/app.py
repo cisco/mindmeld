1db902b8f75c4a551ce5ece456a77e980e89e725
J.J. Jackson <jj@expectlabs.com>
2017-03-08 20:32:08 -0800

    Kwik-E-Mart: Use question answerer

--- tests/kwik-e-mart/app.py
+++ tests/kwik-e-mart/app.py
@@ -25,21 +25,45 @@ def say_goodbye(context, slots, responder):
 
 @app.handle(intent='get_store_hours')
 def send_store_hours(context, slots, responder):
-    stores = [e for e in context['entities'] if e['type'] == 'location']
-    if stores:
-        slots['location'] = stores[0]['value']
-        responder.reply('The {location} Kwik-E-Mart opens at {{open_time}} and '
-                        'closes at {{close_time}} {{date}}.')
+    active_store = None
+    store_entity = [e for e in context['entities'] if e['type'] == 'location']
+    if store_entity:
+        stores = app.question_answerer.get(index='stores', id=store_entity['value']['id'])
+        try:
+            active_store = stores[0]
+        except IndexError:
+            # No active store... continue
+            # TODO: we probably need a better response here
+            pass
+    elif 'target_store' in context['frame']:
+        active_store = context['frame']['target_store']
+
+    if active_store:
+        slots['store_name'] = active_store['store_name']
+        slots['open_time'] = active_store['open_time']
+        slots['close_time'] = active_store['close_time']
+        responder.reply('The {store_name} Kwik-E-Mart opens at {open_time} and '
+                        'closes at {close_time}.')
         return
+
     responder.prompt('Which store would you like to know about?')
 
 
-@app.handle(intent='get_nearest_store')
+@app.handle(intent='find_nearest_store')
 def send_nearest_store(context, slots, responder):
-    # loc = context['session']['location']
-    # stores = qa.get(index='stores', sort='location', current_location=loc)
-    # slots['store_name'] = stores[0]['name']
-    responder.reply('Your nearest Kwik-E-Mart is located at {{location}}.')
+    try:
+        user_location = context['request']['session']['location']
+    except KeyError:
+        # request and session should always be here so assume location is the problem
+        responder.reply("I'm not sure. You haven't told me where you are!")
+        return
+
+    stores = app.question_answerer.get(index='stores', sort='location', location=user_location)
+    target_store = stores[0]
+    slots['store_name'] = target_store['store_name']
+
+    context['frame']['target_store'] = target_store
+    responder.reply('Your nearest Kwik-E-Mart is located at {store_name}.')
 
 
 if __name__ == '__main__':
