dc009beaaaf9377af05cf4c65c4e57b0ffc2b753
J.J. Jackson <jj@expectlabs.com>
2017-03-09 22:55:09 -0800

    Add config_logs helper

--- mmworkbench/__init__.py
+++ mmworkbench/__init__.py
@@ -2,11 +2,13 @@
 from __future__ import unicode_literals
 
 from .app import Application
+from .core import config_logs
 from .dialogue import Conversation
 from .processor.nlp import NaturalLanguageProcessor
+
 
 __author__ = 'MindMeld, Inc.'
 __email__ = 'contact@mindmeld.com'
 __version__ = '3.0.0.dev'
 
-__all__ = ['Application', 'Conversation', 'NaturalLanguageProcessor']
+__all__ = ['config_logs', 'Application', 'Conversation', 'NaturalLanguageProcessor']
