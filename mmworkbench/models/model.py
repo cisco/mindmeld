e2263d7bac698dc2a37cd667efc56df823e8e8d7
J.J. Jackson <jj@expectlabs.com>
2017-03-11 12:33:20 -0800

    Add encoding to model.py

--- mmworkbench/models/model.py
+++ mmworkbench/models/model.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """This module contains base classes for models defined in the models subpackage."""
 from __future__ import absolute_import
 from __future__ import unicode_literals
