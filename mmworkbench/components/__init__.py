67a391b24e24ad033cf03d4d5cae08e1c7e3c12b
Marvin Huang <marvin@expectlabs.com>
2017-05-04 15:56:06 -0700

    initial changes for entity resolver enhancement.

--- mmworkbench/components/__init__.py
+++ mmworkbench/components/__init__.py
@@ -5,4 +5,4 @@ from .nlp import NaturalLanguageProcessor
 from .question_answerer import QuestionAnswerer
 
 
-__all__ = ['Conversation', 'DialogueManager', 'NaturalLanguageProcessor', 'QuestionAnswerer']
+__all__ = ['Conversation', 'DialogueManager', 'NaturalLanguageProcessor', 'QuestionAnswerer', 'EntityResolver']
--- mmworkbench/components/entity_resolver.py
+++ mmworkbench/components/entity_resolver.py
@@ -7,16 +7,170 @@ from builtins import object
 
 import copy
 import logging
+import os
+import json
 
 from ..core import Entity
 
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import streaming_bulk
+
 logger = logging.getLogger(__name__)
+
+DOC_TYPE = "document"
 
 
 class EntityResolver(object):
     """An entity resolver is used to resolve entities in a given query to their canonical values
     (usually linked to specific entries in a knowledge base).
     """
+    ES_SYNONYM_INDEX_PREFIX = "synonym"
+
+    DEFAULT_SYNC_ES_MAPPING = {
+        "mappings": {
+            "document": {
+                "properties": {
+                    "cname": {
+                        "type": "text",
+                        "fields": {
+                            "raw": {
+                                "type": "keyword",
+                                "ignore_above": 256
+                            },
+                            "normalized_keyword": {
+                                "type": "text",
+                                "analyzer": "keyword_match_analyzer"
+                            }
+                        },
+                        "analyzer": "default_analyzer"
+                    },
+                    "id": {
+                        "type": "keyword"
+                    },
+                    "whitelist": {
+                        "type": "text",
+                        "fields": {
+                            "raw": {
+                                "type": "keyword",
+                                "ignore_above": 256
+                            },
+                            "normalized_keyword": {
+                                "type": "text",
+                                "analyzer": "keyword_match_analyzer"
+                            }
+                        },
+                        "analyzer": "default_analyzer"
+                    }
+                }
+            }
+        },
+        "settings": {
+            "analysis": {
+                "filter": {
+                    "token_shingle": {
+                        "max_shingle_size": "4",
+                        "min_shingle_size": "2",
+                        "output_unigrams": "true",
+                        "type": "shingle"
+                    },
+                    "autocomplete_filter": {
+                        "type": "edge_ngram",
+                        "min_gram": "4",
+                        "max_gram": "20"
+                    }
+                },
+                "analyzer": {
+                    "default_analyzer": {
+                        "filter": [
+                            "lowercase",
+                            "asciifolding",
+                            "token_shingle",
+                            "autocomplete_filter"
+                        ],
+                        "char_filter": [
+                            "remove_comma",
+                            "remove_tm_and_r",
+                            "remove_loose_apostrophes",
+                            "space_possessive_apostrophes",
+                            "remove_special_beginning",
+                            "remove_special_end",
+                            "remove_special1",
+                            "remove_special2",
+                            "remove_special3"
+                        ],
+                        "type": "custom",
+                        "tokenizer": "whitespace"
+                    },
+                    "keyword_match_analyzer": {
+                        "filter": [
+                            "lowercase",
+                            "asciifolding"
+                        ],
+                        "char_filter": [
+                            "remove_comma",
+                            "remove_tm_and_r",
+                            "remove_loose_apostrophes",
+                            "space_possessive_apostrophes",
+                            "remove_special_beginning",
+                            "remove_special_end",
+                            "remove_special1",
+                            "remove_special2",
+                            "remove_special3"
+                        ],
+                        "type": "custom",
+                        "tokenizer": "keyword"
+                    }
+                },
+                "char_filter": {
+                    "remove_comma": {
+                        "pattern": ",",
+                        "type": "pattern_replace",
+                        "replacement": ""
+                    },
+                    "remove_loose_apostrophes": {
+                        "pattern": " '|' ",
+                        "type": "pattern_replace",
+                        "replacement": ""
+                    },
+                    "remove_special2": {
+                        "pattern": "([\\p{N}]+)[^\\p{L}\\p{N}&']+(?=[\\p{L}\\s]+)",
+                        "type": "pattern_replace",
+                        "replacement": "$1 "
+                    },
+                    "remove_tm_and_r": {
+                        "pattern": "™|®",
+                        "type": "pattern_replace",
+                        "replacement": ""
+                    },
+                    "remove_special3": {
+                        "pattern": "([\\p{L}]+)[^\\p{L}\\p{N}&']+(?=[\\p{L}]+)",
+                        "type": "pattern_replace",
+                        "replacement": "$1 "
+                    },
+                    "remove_special1": {
+                        "pattern": "([\\p{L}]+)[^\\p{L}\\p{N}&']+(?=[\\p{N}\\s]+)",
+                        "type": "pattern_replace",
+                        "replacement": "$1 "
+                    },
+                    "remove_special_end": {
+                        "pattern": "[^\\p{L}\\p{N}&']+$",
+                        "type": "pattern_replace",
+                        "replacement": ""
+                    },
+                    "space_possessive_apostrophes": {
+                        "pattern": "([^\\p{N}\\s]+)'s ",
+                        "type": "pattern_replace",
+                        "replacement": "$1 's "
+                    },
+                    "remove_special_beginning": {
+                        "pattern": "^[^\\p{L}\\p{N}\\p{Sc}&']+",
+                        "type": "pattern_replace",
+                        "replacement": ""
+                    }
+                }
+            }
+        }
+    }
 
     def __init__(self, resource_loader, entity_type):
         """Initializes an entity resolver
@@ -31,53 +185,142 @@ class EntityResolver(object):
 
         self._mapping = None
         self._is_system_entity = Entity.is_system_entity(self.type)
+        self.__es_client = None
+        self._es_index_name = EntityResolver.ES_SYNONYM_INDEX_PREFIX + "_" + entity_type
+
+    @property
+    def _es_client(self):
+        # Lazily connect to Elasticsearch
+        if self.__es_client is None:
+            self.__es_client = self._create_es_client()
+        return self.__es_client
 
     @staticmethod
-    def process_mapping(entity_type, mapping, normalizer):
-        """
-        Description
+    def _create_es_client(es_host=None, es_user=None, es_pass=None):
+        es_host = es_host or os.environ.get('MM_ES_HOST')
+        es_user = es_user or os.environ.get('MM_ES_USERNAME')
+        es_pass = es_pass or os.environ.get('MM_ES_PASSWORD')
+
+        http_auth = (es_user, es_pass) if es_user and es_pass else None
+        es_client = Elasticsearch(es_host, http_auth=http_auth, request_timeout=60, timeout=60)
+        return es_client
+
+    @classmethod
+    def create_index(cls, index_name, es_client=None):
+        """Creates a new index in the knowledge base.
 
         Args:
-            entity_type: The entity type associated with this entity resolver
-            mapping: Description
-            normalizer: Description
+            index_name (str): The name of the new index to be created
+            es_host (str): The Elasticsearch host server
+            es_client: Description
         """
-        item_map = {}
-        syn_map = {}
-        seen_ids = []
-        for item in mapping:
-            cname = item['cname']
-            item_id = item.get('id')
-            if cname in item_map:
-                msg = 'Canonical name {!r} specified in {!r} entity map multiple times'
-                logger.debug(msg.format(cname, entity_type))
-            if item_id:
-                if item_id in seen_ids:
-                    msg = 'Item id {!r} specified in {!r} entity map multiple times'
-                    raise ValueError(msg.format(item_id, entity_type))
-                seen_ids.append(item_id)
+        es_client = es_client or cls._create_es_client()
 
-            aliases = [cname] + item.pop('whitelist', [])
-            items_for_cname = item_map.get(cname, [])
-            items_for_cname.append(item)
-            item_map[cname] = items_for_cname
-            for alias in aliases:
-                norm_alias = normalizer(alias)
-                if norm_alias in syn_map:
-                    msg = 'Synonym {!r} specified in {!r} entity map multiple times'
-                    logger.debug(msg.format(cname, entity_type))
-                cnames_for_syn = syn_map.get(norm_alias, [])
-                cnames_for_syn.append(cname)
-                syn_map[norm_alias] = list(set(cnames_for_syn))
+        mapping = EntityResolver.DEFAULT_SYNC_ES_MAPPING
 
-        return {'items': item_map, 'synonyms': syn_map}
+        if not es_client.indices.exists(index=index_name):
+            logger.info("Creating index '{}'".format(index_name))
+            es_client.indices.create(index_name, body=mapping)
+        else:
+            logger.error("Index '{}' already exists.".format(index_name))
+
+    @classmethod
+    def ingest_synonym(cls, index_name, mapping, es_client=None):
+        """Loads documents from disk into the specified index in the knowledge base. If an index
+        with the specified name doesn't exist, a new index with that name will be created in the
+        knowledge base.
+
+        Args:
+            index_name (str): The name of the new index to be created
+            data_file (str): The path to the data file containing the documents to be imported
+                into the knowledge base index
+            es_host (str): The Elasticsearch host server
+            es_client: Description
+        """
+        es_client = es_client or cls._create_es_client()
+
+        # with open(data_file) as data_fp:
+        #     data = json.load(data_fp)
+
+        def _doc_generator(docs):
+            for doc in docs:
+                base = {'_id': doc['id']}
+                base.update(doc)
+                yield base
+
+        # create index if specified index does not exist
+        if not es_client.indices.exists(index=index_name):
+            EntityResolver.create_index(index_name, es_client=es_client)
+
+        count = 0
+        for okay, result in streaming_bulk(es_client, _doc_generator(mapping), index=index_name,
+                                           doc_type=DOC_TYPE, chunk_size=50):
+
+            action, result = result.popitem()
+            doc_id = '/%s/%s/%s' % (index_name, DOC_TYPE, result['_id'])
+            # process the information from ES whether the document has been
+            # successfully indexed
+            if not okay:
+                logger.error('Failed to %s document %s: %r', action, doc_id, result)
+            else:
+                count += 1
+                logger.debug('Loaded document: %s', doc_id)
+        logger.info('Loaded %s document%s', count, '' if count == 1 else 's')
+
+    # @staticmethod
+    # def process_mapping(entity_type, mapping, normalizer):
+    #     """
+    #     Description
+    #
+    #     Args:
+    #         entity_type: The entity type associated with this entity resolver
+    #         mapping: Description
+    #         normalizer: Description
+    #     """
+    #     item_map = {}
+    #     syn_map = {}
+    #     seen_ids = []
+    #     for item in mapping:
+    #         cname = item['cname']
+    #         item_id = item.get('id')
+    #         if cname in item_map:
+    #             msg = 'Canonical name {!r} specified in {!r} entity map multiple times'
+    #             logger.debug(msg.format(cname, entity_type))
+    #         if item_id:
+    #             if item_id in seen_ids:
+    #                 msg = 'Item id {!r} specified in {!r} entity map multiple times'
+    #                 raise ValueError(msg.format(item_id, entity_type))
+    #             seen_ids.append(item_id)
+    #
+    #         aliases = [cname] + item.pop('whitelist', [])
+    #         items_for_cname = item_map.get(cname, [])
+    #         items_for_cname.append(item)
+    #         item_map[cname] = items_for_cname
+    #         for alias in aliases:
+    #             norm_alias = normalizer(alias)
+    #             if norm_alias in syn_map:
+    #                 msg = 'Synonym {!r} specified in {!r} entity map multiple times'
+    #                 logger.debug(msg.format(cname, entity_type))
+    #             cnames_for_syn = syn_map.get(norm_alias, [])
+    #             cnames_for_syn.append(cname)
+    #             syn_map[norm_alias] = list(set(cnames_for_syn))
+    #
+    #     return {'items': item_map, 'synonyms': syn_map}
 
     def fit(self):
         """Loads an entity mapping file (if one exists) or trains a machine-learned entity
         resolution model using the provided training examples"""
         if not self._is_system_entity:
             mapping = self._resource_loader.get_entity_map(self.type)
-            self._mapping = self.process_mapping(self.type, mapping, self._normalizer)
+            #self._mapping = self.process_mapping(self.type, mapping, self._normalizer)
+
+            # create index if specified index does not exist
+            # TODO: refactor things around ES calls.
+            if not self._es_client.indices.exists(index=self._es_index_name):
+                EntityResolver.create_index(self._es_index_name, es_client=self._es_client)
+
+            print ('importing to ES')
+            EntityResolver.ingest_synonym(self._es_index_name, mapping)
 
     def predict(self, entity):
         """Predicts the resolved value(s) for the given entity using the loaded entity map or the
@@ -93,25 +336,79 @@ class EntityResolver(object):
             # system entities are already resolved
             return entity.value
 
+        # TODO: revisit the normalization behavior
         normed = self._normalizer(entity.text)
-        try:
-            cnames = self._mapping['synonyms'][normed]
-        except KeyError:
-            logger.warning('Failed to resolve entity %r for type %r', entity.text, entity.type)
-            return entity.text
 
-        if len(cnames) > 1:
-            logger.info('Multiple possible canonical names for %r entity for type %r',
-                        entity.text, entity.type)
+        # try:
+        #     cnames = self._mapping['synonyms'][normed]
+        # except KeyError:
+        #     logger.warning('Failed to resolve entity %r for type %r', entity.text, entity.type)
+        #     return entity.text
+        #
+        # if len(cnames) > 1:
+        #     logger.info('Multiple possible canonical names for %r entity for type %r',
+        #                 entity.text, entity.type)
+        #
+        # values = []
+        # for cname in cnames:
+        #     for item in self._mapping['items'][cname]:
+        #         item_value = copy.copy(item)
+        #         item_value.pop('whitelist', None)
+        #         values.append(item_value)
+        #
+        # return values
 
-        values = []
-        for cname in cnames:
-            for item in self._mapping['items'][cname]:
-                item_value = copy.copy(item)
-                item_value.pop('whitelist', None)
-                values.append(item_value)
+        # ES query for finding exact match on CNAME and synonyms
+        exact_match_query = {
+            "query": {
+                "bool": {
+                    "should": [
+                        {
+                            "match": {
+                                "whitelist.normalized_keyword": {
+                                    "query": normed
+                                }
+                            }
+                        },
+                        {
+                            "match": {
+                                "cname.normalized_keyword": {
+                                    "query": normed
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        }
 
-        return values
+        # ES query for finding exact match on CNAME and synonyms
+        full_text_query = {
+            "query": {
+                "bool": {
+                    "should": [
+                        {
+                            "match": {
+                                "whitelist": {
+                                    "query": normed
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+
+        response = self._es_client.search(index=self._es_index_name, body=exact_match_query)
+        results = [hit['_source'] for hit in response['hits']['hits']]
+
+        # no exact match on CNAME or synonym names found.
+        # Continue to fall back to find matches based on text relevance score.
+        if len(results) == 0:
+            response = self._es_client.search(index=self._es_index_name, body=full_text_query)
+            results = [hit['_source'] for hit in response['hits']['hits']]
+
+        return results
 
     def predict_proba(self, entity):
         """Runs prediction on a given entity and generates multiple hypotheses with their
