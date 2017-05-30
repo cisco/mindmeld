6659c7652d843a17f95a12fb4fedc129c7a7c9c6
J.J. Jackson <jj@expectlabs.com>
2017-05-30 16:49:05 -0700

    Fix maxent model

--- mmworkbench/models/__init__.py
+++ mmworkbench/models/__init__.py
@@ -7,9 +7,12 @@ from .model import ModelConfig
 from . import standard_models
 from . import sequence_models
 from . import query_features
+from . import entity_features
 
 from .helpers import (create_model, QUERY_EXAMPLE_TYPE, ENTITY_EXAMPLE_TYPE, CLASS_LABEL_TYPE,
                       ENTITIES_LABEL_TYPE)
 
-__all__ = ['ModelConfig', 'standard_models', 'sequence_models', 'query_features', 'create_model',
+__all__ = ['ModelConfig', 'standard_models', 'sequence_models',
+           'query_features', 'entity_features',
+           'create_model',
            'QUERY_EXAMPLE_TYPE', 'ENTITY_EXAMPLE_TYPE', 'CLASS_LABEL_TYPE', 'ENTITIES_LABEL_TYPE']
