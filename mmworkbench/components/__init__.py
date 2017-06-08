ce5497a9635ab17961cf7c9d23bbe858a93da928
Arushi Raghuvanshi <arushi@mindmeld.com>
2017-06-08 12:21:14 -0700

    fixed lint error

--- mmworkbench/components/__init__.py
+++ mmworkbench/components/__init__.py
@@ -3,6 +3,8 @@
 from .dialogue import Conversation, DialogueManager
 from .nlp import NaturalLanguageProcessor
 from .question_answerer import QuestionAnswerer
+from .entity_resolver import EntityResolver
 
 
-__all__ = ['Conversation', 'DialogueManager', 'NaturalLanguageProcessor', 'QuestionAnswerer', 'EntityResolver']
+__all__ = ['Conversation', 'DialogueManager', 'NaturalLanguageProcessor', 'QuestionAnswerer',
+           'EntityResolver']
