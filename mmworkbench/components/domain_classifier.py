ce9976ea93be80b5e66f5b6c5c040f80e088a978
vijay <vijay120@gmail.com>
2017-06-26 16:20:54 -0700

    refactored more code

--- mmworkbench/components/domain_classifier.py
+++ mmworkbench/components/domain_classifier.py
@@ -84,10 +84,8 @@ class DomainClassifier(Classifier):
             ModelEvaluation: A ModelEvaluation object that contains evaluation results
         """
         if not self._model:
-            logger.warning('Model does not exist. You must fit or load the model before '
-                           'running evaluate.')
+            logger.error('You must fit or load the model before running evaluate.')
             return
-
         gazetteers = self._resource_loader.get_gazetteers()
         self._model.register_resources(gazetteers=gazetteers)
         queries, classes = self._get_queries_and_labels(queries, label_set='heldout')
