655d3cf75d1a4a63d1578024e24815c7f89a28f0
jjjacksn <jj@mindmeld.com>
2017-07-07 18:10:34 -0700

    Bump version: 3.0.0.dev → 3.0.0rc

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ test_requirements = [
 
 setup(
     name='mmworkbench',
-    version='3.0.0.dev',
+    version='3.0.0rc',
     description="A Python module for building natural language processing models.",
     long_description=readme + '\n\n' + history,
     author="MindMeld, Inc.",
