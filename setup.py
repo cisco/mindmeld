882946260fbe335954d97e16a2bd171fdef621eb
Minh-Tue Vo <minhtue@cisco.com>
2017-08-29 15:45:19 -0700

    Bump version: 3.0.1beta → 3.0.1rc

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ test_requirements = [
 
 setup(
     name='mmworkbench',
-    version='3.0.1beta',
+    version='3.0.1rc',
     description="A Python module for building natural language processing models.",
     long_description=history,
     author="MindMeld, Inc.",
