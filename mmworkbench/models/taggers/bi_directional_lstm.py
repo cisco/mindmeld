e3dad3eb97ee6ea0afbc14b48c6d50ae96f38449
Vijay Ramakrishnan <vijayrk@cisco.com>
2017-08-30 19:48:34 +0530

    fixed dropout

--- mmworkbench/models/taggers/bi_directional_lstm.py
+++ mmworkbench/models/taggers/bi_directional_lstm.py
@@ -25,7 +25,9 @@ class LstmNetwork:
                  embedding_matrix,
                  token_embedding_dimension,
                  token_pretrained_embedding_filepath,
-                 dropout_rate,
+                 dense_keep_probability,
+                 lstm_input_keep_prob,
+                 lstm_output_keep_prob,
                  labels_dict,
                  embedding_gaz_matrix,
                  gaz_features):
@@ -44,35 +46,101 @@ class LstmNetwork:
         self.embedding_matrix = embedding_matrix
         self.token_embedding_dimension = token_embedding_dimension
         self.token_pretrained_embedding_filepath = token_pretrained_embedding_filepath
-        self.dropout_rate = dropout_rate
+        self.dense_keep_probability = dense_keep_probability
+        self.lstm_input_keep_prob = lstm_input_keep_prob
+        self.lstm_output_keep_prob = lstm_output_keep_prob
         self.labels_dict = labels_dict
         self.embedding_gaz_matrix = embedding_gaz_matrix
         self.gaz_features = gaz_features
 
+        self.word_embedding_dimension = self.embedding_matrix.shape[0]
+        self.word_vocab_size = self.embedding_matrix.shape[1]
         self.input = tf.placeholder(tf.int32, [None, self.padding_length])
         self.gaz_input = tf.placeholder(tf.int32, [None, self.padding_length])
-        self.embedding_matrix_tensor = tf.placeholder(tf.float32,
-                                                      [None, self.token_embedding_dimension])
-        self.embedding_matrix_gaz_tensor = tf.placeholder(tf.float32,
-                                                          [None,
-                                                           self.embedding_gaz_matrix.shape[1]])
-        self.label_tensor = tf.placeholder(tf.int32,
-                                           [None, int(self.padding_length), self.output_dimension])
-        self.sequence_lengths = tf.placeholder(tf.int32, shape=[None],
-                                               name="sequence_lengths")
-        self.tf_dropout_rate = tf.placeholder(tf.float32, shape=None)
+        self.embedding_matrix_tensor = tf.placeholder(tf.float32, [None, self.token_embedding_dimension])
+        self.embedding_matrix_gaz_tensor = tf.placeholder(tf.float32, [None, self.embedding_gaz_matrix.shape[1]])
+        self.label_tensor = tf.placeholder(tf.int32, [None, int(self.padding_length), self.output_dimension])
+        self.sequence_lengths = tf.placeholder(tf.int32, shape=[None], name="sequence_lengths")
 
-        input_tensor = tf.nn.embedding_lookup(self.embedding_matrix_tensor, self.input)
-        gaz_tensor = tf.nn.embedding_lookup(self.embedding_matrix_gaz_tensor, self.gaz_input)
+        # input_tensor = tf.nn.embedding_lookup(self.embedding_matrix_tensor, self.input)
+        # gaz_tensor = tf.nn.embedding_lookup(self.embedding_matrix_gaz_tensor, self.gaz_input)
+        #
+        # x_concat = tf.concat([input_tensor, gaz_tensor], axis=-1)
 
-        x_concat = tf.concat([input_tensor, gaz_tensor], axis=-1)
-
-        self.output_tensor = self._construct_network(x_concat)
+        combined_embedding = self._construct_embedding_network()
+        self.output_tensor = self._construct_network(combined_embedding)
 
         self.optimizer, self.cost = self._define_optimizer_and_cost(
             self.output_tensor, self.label_tensor, self.sequence_lengths)
 
         self.accuracy = self._calculate_accuracy(self.output_tensor, self.label_tensor)
+
+    def _construct_embedding_network(self):
+        """
+        Constructs all aspects of the network pre-LSTM.
+        :return: just word embedding, character level word embedding, combined (including gaz or ortho if used),
+        gaz representation (for optional appending to LSTM output)
+        """
+
+        # Sequence lengths tensor
+        batch_size = tf.shape(self.sequence_lengths)[0]
+
+        # GAZ CONSTRUCTION
+        gaz_one_hot = tf.one_hot(
+            indices=tf.cast(self.gaz_input, tf.int32),
+            depth=self.embedding_gaz_matrix.shape[1],
+            dtype=tf.float32)
+
+        gaz_embedding_matrix_weights = tf.Variable(
+            self.embedding_gaz_matrix,
+            dtype=tf.float32,
+            trainable=False)
+
+        gaz_embedding_matrix_weights = tf.reshape(
+            tf.tile(gaz_embedding_matrix_weights, [batch_size, 1]),
+            [batch_size, self.embedding_gaz_matrix.shape[0],
+             self.embedding_gaz_matrix.shape[1]])
+
+        # Each word gets turned into a binary vector that represents presense in gazetteers
+        gaz_embedding = tf.matmul(gaz_embedding_matrix_weights, gaz_one_hot, transpose_b=True)
+        gaz_embedding = tf.transpose(gaz_embedding, [0, 2, 1])
+
+        dense_gaz_embedding = tf.contrib.layers.fully_connected(gaz_embedding, 100)
+
+        # WORD EMBEDDING CONSTRUCTION
+
+        # Shape: batch_size BY query_padding_length BY word_vocab_size
+        words_one_hot = tf.one_hot(
+            indices=tf.cast(self.input, tf.int32),
+            depth=self.word_vocab_size,
+            dtype=tf.float32)
+
+        word_embedding_matrix_weights = tf.Variable(
+            self.embedding_matrix,
+            dtype=tf.float32,
+            trainable=False)
+
+        # Duplicate/tile the matrix weights batch_size number of times so each example has
+        # the weights to multiply by. Shape: (word_embedding_dimension*batch_size) BY word_vocab_size
+        word_embedding_matrix_weights = tf.tile(word_embedding_matrix_weights, [batch_size, 1])
+
+        # Must reshape so each batch_size dimension has a copy of the word embedding matrix weights
+        # Shape: batch_size BY word_embedding_dimension BY word_vocab_size
+        word_embedding_matrix_weights = tf.reshape(word_embedding_matrix_weights,
+                                                   [batch_size,
+                                                    self.word_embedding_dimension,
+                                                    self.word_vocab_size])
+
+        # Extract the column vector that corresponds to the embedding
+        # Shape: batch_size BY word_embedding_dimension BY query_padding_length
+        word_embedding = tf.matmul(word_embedding_matrix_weights, words_one_hot, transpose_b=True)
+
+        # Transpose because for each batch we want the first dimension to correspond to words, second to embedding
+        word_embedding = tf.transpose(word_embedding, [0, 2, 1])
+
+        combined_embedding = tf.concat([word_embedding, dense_gaz_embedding], axis=2)
+
+        return combined_embedding
 
     def _define_optimizer_and_cost(self, output_tensor, label_tensor, sequence_lengths):
         """ This function defines the optimizer and cost function of the LSTM model
@@ -133,6 +201,14 @@ class LstmNetwork:
         lstm_cell_back = tf.contrib.rnn.CoupledInputForgetGateLSTMCell(
             n_hidden, forget_bias=1.0, initializer=initializer, state_is_tuple=True)
 
+        lstm_cell_for = tf.contrib.rnn.DropoutWrapper(
+            lstm_cell_for, input_keep_prob=self.lstm_input_keep_prob, output_keep_prob=self.lstm_output_keep_prob
+        )
+
+        lstm_cell_back = tf.contrib.rnn.DropoutWrapper(
+            lstm_cell_back, input_keep_prob=self.lstm_input_keep_prob, output_keep_prob=self.lstm_output_keep_prob
+        )
+
         initial_cell_state = tf.get_variable(
             "initial_cell_state", shape=[1, n_hidden], dtype=tf.float32, initializer=initializer)
 
@@ -163,7 +239,7 @@ class LstmNetwork:
 
         output = tf.concat([output_fw, output_bw], axis=-1)
         output = tf.reshape(output, [-1, 2 * n_hidden])
-        output = tf.nn.dropout(output, float(self.dropout_rate))
+        output = tf.nn.dropout(output, float(self.dense_keep_probability))
 
         weights = tf.get_variable("weights_out", shape=[2 * n_hidden, self.output_dimension],
                                   dtype="float32", initializer=tf.random_normal_initializer())
@@ -174,16 +250,19 @@ class LstmNetwork:
         output_tensor = tf.matmul(output, weights) + biases
         return output_tensor
 
-    def construct_feed_dictionary(self, batch_examples, batch_labels, batch_gaz, batch_seq_len):
-        return {
+    def construct_feed_dictionary(self, batch_examples, batch_gaz, batch_seq_len, batch_labels=list()):
+        return_dict = {
             self.input: batch_examples,
-            self.label_tensor: batch_labels,
             self.sequence_lengths: batch_seq_len,
             self.gaz_input: batch_gaz,
             self.embedding_matrix_tensor: self.embedding_matrix,
-            self.embedding_matrix_gaz_tensor: self.embedding_gaz_matrix,
-            self.tf_dropout_rate: self.dropout_rate
+            self.embedding_matrix_gaz_tensor: self.embedding_gaz_matrix
         }
+
+        if len(batch_labels) > 0:
+            return_dict[self.label_tensor] = batch_labels
+
+        return return_dict
 
     def fit(self, examples, labels):
         """ Main function that iterates through the data, feeds batches to the tensorflow session graph and fits
@@ -214,7 +293,7 @@ class LstmNetwork:
                 if batch % int(self.display_step) == 0:
                     acc, loss = self.session.run([self.accuracy, self.cost],
                                                  feed_dict=self.construct_feed_dictionary(
-                                                     batch_examples, batch_labels, batch_gaz, seq_len))
+                                                     batch_examples, batch_gaz, seq_len, batch_labels))
                     accuracy = acc / (len(batch_examples) * 1.0)
 
                     print("Iteration number " + str(batch * batch_size) + ", Minibatch Loss= " +
@@ -227,7 +306,7 @@ class LstmNetwork:
                 else:
                     self.session.run(self.optimizer,
                                      feed_dict=self.construct_feed_dictionary(
-                                         batch_examples, batch_labels, batch_gaz, seq_len))
+                                         batch_examples, batch_gaz, seq_len, batch_labels))
 
         return self
 
@@ -241,17 +320,17 @@ class LstmNetwork:
         """
         gaz = self.gaz_features
         seq_len = np.ones(len(examples)) * int(self.padding_length)
+
+        self.dense_keep_probability = 1.0
+        self.lstm_input_keep_prob = 1.0
+        self.lstm_output_keep_prob = 1.0
+
         output_tensor = self.session.run(
             self.output_tensor,
-            feed_dict={self.input: examples,
-                       self.sequence_lengths: seq_len,
-                       self.gaz_input: gaz,
-                       self.embedding_matrix_tensor: self.embedding_matrix,
-                       self.embedding_matrix_gaz_tensor: self.embedding_gaz_matrix,
-                       self.tf_dropout_rate: 1.0})
+            feed_dict=self.construct_feed_dictionary(examples, gaz, seq_len))
 
-        output_tensor = tf.reshape(output_tensor,
-                                   [-1, int(self.padding_length), self.output_dimension])
+        output_tensor = tf.reshape(
+            output_tensor, [-1, int(self.padding_length), self.output_dimension])
         output_tensor = tf.argmax(output_tensor, 2)
         output_array = self.session.run(output_tensor)
 
