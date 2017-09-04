eeb6c1ed1a32a79d9e00a80cbbcef2ae0b3564c3
J.J. Jackson <jdot@cisco.com>
2017-09-04 09:39:59 -0700

    Bump version: 3.0.2 → 3.0.3

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ test_requirements = [
 
 setup(
     name='mmworkbench',
-    version='3.0.2',
+    version='3.0.3',
     description="A Python module for building natural language processing models.",
     long_description=history,
     author="MindMeld, Inc.",
