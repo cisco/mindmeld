42a4903cb63ee1382ed51d4391e7f1061d304355
Marvin Huang <marvin@expectlabs.com>
2017-09-19 23:02:41 -0700

    Bump version: 3.1.0rc5 → 3.1.0rc6

--- mmworkbench/_version.py
+++ mmworkbench/_version.py
@@ -9,7 +9,7 @@ import logging
 from pkg_resources import DistributionNotFound, VersionConflict
 from .exceptions import WorkbenchVersionError
 
-current = '3.1.0rc5'
+current = '3.1.0rc6'
 
 logger = logging.getLogger(__name__)
 
--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ test_requirements = [
 
 setup(
     name='mmworkbench',
-    version='3.1.0rc5',
+    version='3.1.0rc6',
     description="A Python module for building natural language processing models.",
     long_description=history,
     author="MindMeld, Inc.",
