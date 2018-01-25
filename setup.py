afc8638717b6249d11b32e5040e00c569cd54fb8
J.J. Jackson <jdot@cisco.com>
2018-01-24 16:42:33 -0800

    Add bleach dependency to fix resolution

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
+    'bleach==1.5.0',  # tensorflow -> tensorboard -> bleach, prevents conflicts with jupyter
     'Click~=6.0',
     'click-log==0.1.8',  # check WB3-191
     'elasticsearch>=5.1.0,<=5.4.0',
