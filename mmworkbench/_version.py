578d46ca9b85fc0a81a465744e0bc004e6b6400e
Vijay Ramakrishnan <vijayrk@cisco.com>
2018-05-10 19:48:19 +0530

    Bump version: 3.3.0rc5 → 3.3.0

--- mmworkbench/_version.py
+++ mmworkbench/_version.py
@@ -13,7 +13,7 @@ except ImportError:
 from pkg_resources import DistributionNotFound, VersionConflict
 from .exceptions import WorkbenchVersionError
 
-current = '3.3.0rc5'
+current = '3.3.0'
 
 logger = logging.getLogger(__name__)
 
--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ test_requirements = [
 
 setup(
     name='mmworkbench',
-    version='3.3.0rc5',
+    version='3.3.0',
     description="A Python module for building natural language processing models.",
     long_description=history,
     author="MindMeld, Inc.",
