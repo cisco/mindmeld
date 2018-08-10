92f9774aba4bb412a43b1a156f83f3968f08086e
J.J. Jackson <jdot@cisco.com>
2018-08-10 15:44:13 -0700

    Bump version: 4.0.0dev → 4.0.0alpha

--- mmworkbench/_version.py
+++ mmworkbench/_version.py
@@ -7,7 +7,7 @@ import logging
 from pkg_resources import DistributionNotFound, VersionConflict
 from .exceptions import WorkbenchVersionError
 
-current = '4.0.0dev'
+current = '4.0.0alpha'
 
 logger = logging.getLogger(__name__)
 
