cd766ead441fabba83723f928a15cfd7fa8b7a1b
Varsha Embar <vembar@cisco.com>
2018-10-23 15:43:41 -0700

    adding tests for role classification

---
+++ tests/components/test_role_classifier.py
@@ -0,0 +1,46 @@
+
+import pytest
+import os
+
+from mmworkbench.components import NaturalLanguageProcessor
+
+HOME_ASSISTANT_APP_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                       '../home_assistant')
+
+
+@pytest.fixture(scope='session')
+def home_assistant_app_path():
+    return HOME_ASSISTANT_APP_PATH
+
+
+@pytest.fixture(scope='session')
+def home_assistant_nlp(home_assistant_app_path):
+    """Provides a built processor instance"""
+    nlp = NaturalLanguageProcessor(app_path=home_assistant_app_path)
+    nlp.build()
+    nlp.dump()
+    return nlp
+
+
+test_data_7 = [
+    (
+        ['change alarm from 2 pm to 4 pm', 'times_and_dates', 'change_alarm', 'sys_time'],
+        ['old_time', 'new_time'],
+        ['new_time', 'old_time']
+     )
+]
+
+
+@pytest.mark.parametrize("example,role_order_0,role_order_1", test_data_7)
+def test_role_classifier(home_assistant_nlp, example, role_order_0, role_order_1):
+
+    intent = home_assistant_nlp.domains[example[1]].intents[example[2]]
+    entity_recognizer = intent.entity_recognizer
+    role_classifier = intent.entities[example[3]].role_classifier
+
+    entities = entity_recognizer.predict(example[0])
+    probs = role_classifier.predict_proba(example[0], entities, 0)
+    assert([tup[0] for tup in probs] == role_order_0)
+
+    probs = role_classifier.predict_proba(example[0], entities, 1)
+    assert ([tup[0] for tup in probs] == role_order_1)
---
+++ tests/home_assistant/config.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""This module contains the Home Assistant Blueprint Application"""
+
+INTENT_MODEL_CONFIG = {
+    'model_type': 'text',
+    'model_settings': {
+        'classifier_type': 'logreg'
+    },
+    'param_selection': {
+        'type': 'k-fold',
+        'k': 5,
+        'grid': {
+            'fit_intercept': [True, False],
+            'C': [0.01, 1, 10, 100],
+            'class_bias': [0.7, 0.3, 0]
+        }
+    },
+    'features': {
+        "bag-of-words": {
+            "lengths": [1, 2]
+        },
+        "word-shape": {"lengths": [1]},
+        "sys-candidates": {"entities": ['sys_time', 'sys_number', 'sys_phone-number']},
+    }
+}
+
+DOMAIN_MODEL_CONFIG = {
+    'model_type': 'text',
+    'model_settings': {
+        'classifier_type': 'logreg'
+    },
+    'params': {
+        'C': 10,
+    },
+    'features': {
+        "bag-of-words": {
+            "lengths": [1, 2]
+        },
+        "edge-ngrams": {"lengths": [1, 2]},
+        "in-gaz": {},
+        "exact": {"scaling": 10},
+        "gaz-freq": {},
+        "freq": {"bins": 5}
+    }
+}
