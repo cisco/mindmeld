ab7c159cc01d6bb324d77aee154884aa7c366ef7
Minh-Tue Vo <minhtue@cisco.com>
2018-10-30 18:53:05 -0700

    Bump version: 3.4.1dev0 → 3.4.1

--- mmworkbench/_version.py
+++ mmworkbench/_version.py
@@ -7,7 +7,7 @@ import logging
 from pkg_resources import DistributionNotFound, VersionConflict
 from .exceptions import WorkbenchVersionError
 
-current = '3.4.1dev'
+current = '3.4.1'
 
 logger = logging.getLogger(__name__)
 
