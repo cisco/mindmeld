4fdaa608e970a528956f5e1826b760bdbe1971b5
Vijay Ramakrishnan Thimmaiyah <vijayrk@cisco.com>
2018-11-02 14:24:59 -0700

    Bump version: 4.0.0alpha14 → 4.0.0alpha15

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ test_requirements = [
 
 setup(
     name='mmworkbench',
-    version='4.0.0alpha14',
+    version='4.0.0alpha15',
     description="A Python module for building natural language processing models.",
     long_description=history,
     author="MindMeld, Inc.",
