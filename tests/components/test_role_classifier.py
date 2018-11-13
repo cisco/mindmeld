03606b58a1d57410aca409bd74dde06c655076ac
Varsha Embar <vembar@cisco.com>
2018-11-13 11:09:43 -0800

    adding tests

--- tests/components/test_role_classifier.py
+++ tests/components/test_role_classifier.py
@@ -39,3 +39,26 @@ def test_role_classifier(home_assistant_nlp, example, role_order_0, role_order_1
 
     probs = role_classifier.predict_proba(example[0], entities, 1)
     assert ([tup[0] for tup in probs] == role_order_1)
+
+
+test_data_8 = [
+    (
+        ['change alarm from 2 pm to 4 pm', 'times_and_dates', 'change_alarm', 'sys_time'],
+        ['old_time', 'new_time']
+     )
+]
+
+
+@pytest.mark.parametrize("example,role_type", test_data_8)
+def test_nlp_process_for_roles(home_assistant_nlp, example, role_type):
+    result = home_assistant_nlp.process(example)
+
+    assert(result['entities'][0]['role']['type'] == role_type[0])
+    assert (result['entities'][1]['role']['type'] == role_type[1])
+
+    result = home_assistant_nlp.process(example, verbose=True)
+
+    assert (result['entities'][0]['role']['type'] == role_type[0])
+    assert (result['entities'][1]['role']['type'] == role_type[1])
+    assert (result['entities'][0]['role'].get('confidence', None))
+    assert (result['entities'][1]['role'].get('confidence', None))
