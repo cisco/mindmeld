f87bd4e308f5da0eb2ec25e3d1b5f02e335fec7d
Alex Huang <alhuang10@gmail.com>
2018-12-20 14:42:25 -0800

    Make the entity recognizer config for get_store_hours deterministic

--- tests/kwik_e_mart/config.py
+++ tests/kwik_e_mart/config.py
@@ -64,8 +64,14 @@ ENTITY_RECOGNIZER_CONFIG = {
         'tag_scheme': 'IOB',
         'feature_scaler': 'max-abs'
     },
-    'params': {
-        'C': 100
+    'param_selection': {
+        'type': 'k-fold',
+        'k': 5,
+        'scoring': 'accuracy',
+        'grid': {
+            'penalty': ['l1', 'l2'],
+            'C': [0.01, 1, 100, 10000, 1000000, 100000000]
+        },
     },
     'features': {
         'bag-of-words-seq': {
@@ -91,14 +97,8 @@ TEST_ENTITY_RECOGNIZER_CONFIG = {
         'tag_scheme': 'IOB',
         'feature_scaler': 'max-abs'
     },
-    'param_selection': {
-        'type': 'k-fold',
-        'k': 5,
-        'scoring': 'accuracy',
-        'grid': {
-            'penalty': ['l1', 'l2'],
-            'C': [0.01, 1, 100, 10000, 1000000, 100000000]
-        },
+    'params': {
+        'C': 100
     },
     'features': {
         'bag-of-words-seq': {
