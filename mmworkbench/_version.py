19c6e1e545bdfe836c86c27a1a98329d35856c14
Vijay Ramakrishnan Thimmaiyah <vijayrk@cisco.com>
2018-12-21 17:04:07 -0800

    Bump version: 4.0.0mallard2 → 4.0.0mallard3

--- mmworkbench/_version.py
+++ mmworkbench/_version.py
@@ -9,7 +9,7 @@ from pkg_resources import DistributionNotFound, VersionConflict
 
 from .exceptions import WorkbenchVersionWarning, WorkbenchVersionError
 
-current = '4.0.0mallard2'
+current = '4.0.0mallard3'
 
 logger = logging.getLogger(__name__)
 
