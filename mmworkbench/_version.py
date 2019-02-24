0ae3655599a5116dfb2beaf5d56e62e94e32508d
Minh-Tue Vo <minhtue@cisco.com>
2019-02-24 09:51:56 -0800

    Bump version: 4.0.0rc2 → 4.0.0rc3

--- mmworkbench/_version.py
+++ mmworkbench/_version.py
@@ -9,7 +9,7 @@ from pkg_resources import DistributionNotFound, VersionConflict
 
 from .exceptions import WorkbenchVersionWarning, WorkbenchVersionError
 
-current = '4.0.0rc2'
+current = '4.0.0rc3'
 
 logger = logging.getLogger(__name__)
 
