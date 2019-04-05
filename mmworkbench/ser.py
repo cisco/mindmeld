7945fc15cbdb8aacbc4110859b8e511ab5f8c522
Vijay Ramakrishnan <vijayrk@cisco.com>
2019-04-05 14:46:35 -0700

    Create License (#502)
    Created license and copyright headers

--- mmworkbench/ser.py
+++ mmworkbench/ser.py
@@ -1,4 +1,16 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """This module contains the system entity recognizer."""
 import logging
 import json
@@ -107,9 +119,9 @@ def parse_numerics(sentence, dimensions=None, language='EN', locale='en_US',
     Returns:
         (tuple): A tuple containing:
 
-            * response (list, dict): Response from the System Entity Recognizer service that consists
-            of a list of dicts, each corresponding to a single prediction or just a dict, corresponding
-            to a single prediction.
+            * response (list, dict): Response from the System Entity Recognizer service that
+            consists of a list of dicts, each corresponding to a single prediction or just a
+            dict, corresponding to a single prediction.
 
             * response_code (int): http status code.
     """
