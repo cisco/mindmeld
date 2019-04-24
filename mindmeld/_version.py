1cdcc8e8e20bea734a04db905d795f029e7c4e77
Minh-Tue Vo <minhtue@cisco.com>
2019-04-23 23:21:34 -0700

    Bump version: 4.1.0rc4 → 4.1.0rc5

--- mindmeld/_version.py
+++ mindmeld/_version.py
@@ -21,7 +21,7 @@ from pkg_resources import DistributionNotFound, VersionConflict
 
 from .exceptions import MindMeldVersionWarning, MindMeldVersionError
 
-current = '4.1.0rc4'
+current = '4.1.0rc5'
 
 logger = logging.getLogger(__name__)
 
--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ test_requirements = [
 
 setup(
     name='mindmeld',
-    version='4.1.0rc4',
+    version='4.1.0rc5',
     description="A Conversational AI platform.",
     long_description=readme,
     long_description_content_type='text/markdown',
