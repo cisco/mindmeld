deb7c5c2be61417f5e1dabe326249b2bd3e2a31e
Vijay Ramakrishnan Thimmaiyah <vijayrk@cisco.com>
2019-06-28 18:16:37 -0500

    Bump version: 4.1.3 → 4.1.4

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ test_requirements = [
 
 setup(
     name='mindmeld',
-    version='4.1.3',
+    version='4.1.4',
     description="A Conversational AI platform.",
     long_description=readme,
     long_description_content_type='text/markdown',
