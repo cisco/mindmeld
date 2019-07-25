4b1683ea30521cc12ff5536a2463d53ca06dd2cd
Barret Pickett <mapicket@cisco.com>
2019-07-25 16:32:05 -0500

    Added additional rasa tests

--- tests/converter/rasa.py
+++ tests/converter/rasa.py
@@ -9,8 +9,29 @@ def test_rasa_converter(rasa_project_path, mindmeld_project_path):
     converter = RasaConverter(rasa_project_path, mindmeld_project_path)
     converter.convert_project()
 
-def test_nlp(mindmeld_project_path):
+def test_nlp_greet(mindmeld_project_path):
     nlp = NaturalLanguageProcessor(mindmeld_project_path)
     nlp.build()
     query1 = 'hi there'
     assert nlp.process(query1)['intent'] == 'greet'
+
+def test_nlp_name(mindmeld_project_path):
+    nlp = NaturalLanguageProcessor(mindmeld_project_path)
+    nlp.build()
+    query1 = 'My name is Josh'
+    assert nlp.process(query1)['intent'] == 'name'
+    entity = nlp.process(query1)['entities']
+    assert entity[0]['text'] == 'Josh'
+
+def test_nlp_thanks(mindmeld_project_path):
+    nlp = NaturalLanguageProcessor(mindmeld_project_path)
+    nlp.build()
+    query1 = 'thank you'
+    assert nlp.process(query1)['intent'] == 'thanks'
+
+def test_nlp_goodbye(mindmeld_project_path):
+    nlp = NaturalLanguageProcessor(mindmeld_project_path)
+    nlp.build()
+    query1 = 'See you later'
+    assert nlp.process(query1)['intent'] == 'goodbye'
+
