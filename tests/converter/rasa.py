36d96503f5c1e215dd1bc4f24fed70b9ec82fe82
Barret Pickett <mapicket@cisco.com>
2019-08-01 17:05:01 -0500

    Added pytest fixture to rasa test file

--- tests/converter/rasa.py
+++ tests/converter/rasa.py
@@ -1,6 +1,8 @@
 """This module tests the convertion tool that is used to convert a rasa or diaglogflow
 project into a mindmeld project"""
 
+import pytest
+
 from mindmeld import NaturalLanguageProcessor
 from mindmeld.converter.rasa_converter import RasaConverter
 
@@ -10,48 +12,43 @@ def test_rasa_converter(rasa_project_path, mindmeld_project_path):
     converter.convert_project()
 
 
-def test_nlp_greet(mindmeld_project_path):
+@pytest.fixture
+def rasa_nlp(mindmeld_project_path):
     nlp = NaturalLanguageProcessor(mindmeld_project_path)
     nlp.build()
+    return nlp
+
+
+def test_nlp_greet(rasa_nlp):
     query1 = 'hi there'
-    assert nlp.process(query1)['intent'] == 'greet'
+    assert rasa_nlp.process(query1)['intent'] == 'greet'
 
 
-def test_nlp_name(mindmeld_project_path):
-    nlp = NaturalLanguageProcessor(mindmeld_project_path)
-    nlp.build()
+def test_nlp_name(rasa_nlp):
     query1 = 'My name is Josh'
-    assert nlp.process(query1)['intent'] == 'name'
-    entity = nlp.process(query1)['entities']
+    assert rasa_nlp.process(query1)['intent'] == 'name'
+    entity = rasa_nlp.process(query1)['entities']
     assert entity[0]['text'] == 'Josh'
 
 
-def test_nlp_thanks(mindmeld_project_path):
-    nlp = NaturalLanguageProcessor(mindmeld_project_path)
-    nlp.build()
+def test_nlp_thanks(rasa_nlp):
     query1 = 'thank you'
-    assert nlp.process(query1)['intent'] == 'thanks'
+    assert rasa_nlp.process(query1)['intent'] == 'thanks'
 
 
-def test_nlp_goodbye(mindmeld_project_path):
-    nlp = NaturalLanguageProcessor(mindmeld_project_path)
-    nlp.build()
+def test_nlp_goodbye(rasa_nlp):
     query1 = 'See you later'
-    assert nlp.process(query1)['intent'] == 'goodbye'
+    assert rasa_nlp.process(query1)['intent'] == 'goodbye'
 
 
-def test_nlp_lastname(mindmeld_project_path):
-    nlp = NaturalLanguageProcessor(mindmeld_project_path)
-    nlp.build()
+def test_nlp_lastname(rasa_nlp):
     query1 = 'I am Josh Williams'
-    assert nlp.process(query1)['intent'] == 'lastname'
-    entity = nlp.process(query1)['entities']
+    assert rasa_nlp.process(query1)['intent'] == 'lastname'
+    entity = rasa_nlp.process(query1)['entities']
     assert entity[0]['text'] == 'Josh'
     assert entity[1]['text'] == 'Williams'
 
 
-def test_nlp_deny(mindmeld_project_path):
-    nlp = NaturalLanguageProcessor(mindmeld_project_path)
-    nlp.build()
+def test_nlp_deny(rasa_nlp):
     query1 = 'No, I don\'t want that'
-    assert nlp.process(query1)['intent'] == 'deny'
+    assert rasa_nlp.process(query1)['intent'] == 'deny'
