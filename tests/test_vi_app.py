20b8144fae8c7340107c4f4907fd9fb65b89c756
Derek <derekmpham@gmail.com>
2019-12-18 09:28:09 -0800

    Add test for get_store_hours

--- tests/test_vi_app.py
+++ tests/test_vi_app.py
@@ -2,3 +2,8 @@ def test_greeting(vi_kwik_e_mart_nlp):
     result = vi_kwik_e_mart_nlp.process("Xin chào")
     assert result["domain"] == "store_info"
     assert result["intent"] == "greet"
+
+def test_get_store_hours(vi_kwik_e_mart_nlp):
+	result = vi_kwik_e_mart_nlp.process("Central Plaza sẽ đóng cửa mấy giờ hôm nay?")
+	assert result["domain"] == "store_info"
+	assert result["intent"] == "get_store_hours"
