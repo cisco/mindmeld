e94316d976b80598a8cbda813aee2a0d3f4c07d3
Derek <derekmpham@gmail.com>
2019-12-18 09:38:02 -0800

    Add test for find_nearest_store

--- tests/test_vi_app.py
+++ tests/test_vi_app.py
@@ -3,6 +3,11 @@ def test_greeting(vi_kwik_e_mart_nlp):
     assert result["domain"] == "store_info"
     assert result["intent"] == "greet"
 
+def test_find_nearest_store(vi_kwik_e_mart_nlp):
+	result = vi_kwik_e_mart_nlp.process("Cửa hàng nào gần nhất tôi vậy?")
+	assert result["domain"] == "store_info"
+	assert result["intent"] == "find_nearest_store"
+
 def test_get_store_hours(vi_kwik_e_mart_nlp):
 	result = vi_kwik_e_mart_nlp.process("Central Plaza sẽ đóng cửa mấy giờ hôm nay?")
 	assert result["domain"] == "store_info"
