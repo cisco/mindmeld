5524d2bcbb9372a4a4797d0362af55e320f14475
Vijay Ramakrishnan Thimmaiyah <vijayrk@cisco.com>
2020-01-10 16:36:20 -0800

    updated mindmeld from v4.2.3 to v4.2.4

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ test_requirements = [
 
 setup(
     name="mindmeld",
-    version="4.2.3",
+    version="4.2.4",
     description="A Conversational AI platform.",
     long_description=readme,
     long_description_content_type="text/markdown",
