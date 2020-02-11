50f73855337d1b6bf859da1fae326843d624effd
Vijay Ramakrishnan <vijayrk@cisco.com>
2020-02-11 15:13:03 -0800

    upgraded flask (#115)

--- tests/test_request.py
+++ tests/test_request.py
@@ -5,63 +5,63 @@ from mindmeld.components.request import FrozenParams, Params, Request
 
 
 @pytest.fixture
-def request():
+def sample_request():
     return Request(
         domain="some_domain", intent="some_intent", entities=(), text="some_text"
     )
 
 
-def test_domain(request):
+def test_domain(sample_request):
     with pytest.raises(FrozenInstanceError):
-        request.domain = "new_domain"
+        sample_request.domain = "new_domain"
 
 
-def test_intent(request):
+def test_intent(sample_request):
     with pytest.raises(FrozenInstanceError):
-        request.intent = "new_intent"
+        sample_request.intent = "new_intent"
 
 
-def test_entities(request):
+def test_entities(sample_request):
     with pytest.raises(FrozenInstanceError):
-        request.entities = ("some_entity",)
+        sample_request.entities = ("some_entity",)
 
 
-def test_text(request):
+def test_text(sample_request):
     with pytest.raises(FrozenInstanceError):
-        request.text = "some_text"
+        sample_request.text = "some_text"
 
 
-def test_frame(request):
+def test_frame(sample_request):
     with pytest.raises(FrozenInstanceError):
-        request.frame = {"key": "value"}
+        sample_request.frame = {"key": "value"}
 
 
-def test_params(request):
+def test_params(sample_request):
     with pytest.raises(FrozenInstanceError):
-        request.params = {"key": "value"}
+        sample_request.params = {"key": "value"}
 
 
-def test_context(request):
+def test_context(sample_request):
     with pytest.raises(FrozenInstanceError):
-        request.context = {"key": "value"}
+        sample_request.context = {"key": "value"}
 
 
-def test_nbest(request):
+def test_nbest(sample_request):
     with pytest.raises(FrozenInstanceError):
-        request.confidences = {"key": "value"}
-
-    with pytest.raises(FrozenInstanceError):
-        request.nbest_transcripts_text = ["some_text"]
+        sample_request.confidences = {"key": "value"}
 
     with pytest.raises(FrozenInstanceError):
-        request.nbest_transcripts_entities = [{"key": "value"}]
+        sample_request.nbest_transcripts_text = ["some_text"]
 
     with pytest.raises(FrozenInstanceError):
-        request.nbest_aligned_entities = [{"key": "value"}]
+        sample_request.nbest_transcripts_entities = [{"key": "value"}]
+
+    with pytest.raises(FrozenInstanceError):
+        sample_request.nbest_aligned_entities = [{"key": "value"}]
 
 
-def test_immutability_of_request_and_params():
-    """Test the immutability of the request and params objects"""
+def test_immutability_of_sample_request_and_params():
+    """Test the immutability of the sample_request and params objects"""
     with pytest.raises(FrozenInstanceError):
         params = FrozenParams()
         params.allowed_intents = []
