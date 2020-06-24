97c50d0f50600b73793c1de3530f71d01d977966
Minh-Tue Vo <minhtue@cisco.com>
2020-06-24 15:01:43 -0700

    Rename TestConversation to ConversationTestHelper since the pattern "Test*" is reserved for test suites

--- tests/test_conversation.py
+++ tests/test_conversation.py
@@ -1,8 +1,8 @@
-from mindmeld.test import TestConversation
+from mindmeld.test import ConversationTestHelper
 
 
 def test_conversation(kwik_e_mart_app_path):
-    conv = TestConversation(app_path=kwik_e_mart_app_path)
+    conv = ConversationTestHelper(app_path=kwik_e_mart_app_path)
     conv.process("hi")
     conv.assert_text(
         "Hello. I can help you find store hours for your local Kwik-E-Mart."
