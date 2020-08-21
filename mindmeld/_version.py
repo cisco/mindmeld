d5dd454e7c5d070dafe8fde261bfded1c4fc7a75
Minh-Tue Vo <minhtue@cisco.com>
2020-08-21 16:12:13 -0700

    Update to MindMeld 4.3.2rc1

--- mindmeld/_version.py
+++ mindmeld/_version.py
@@ -14,7 +14,7 @@
 """Defines mindmeld version information"""
 from __future__ import absolute_import, unicode_literals
 
-current = "4.3.1"
+current = "4.3.2rc1"
 
 
 def get_mm_version():
--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ test_requirements = [
 
 setup(
     name="mindmeld",
-    version="4.3.1",
+    version="4.3.2rc1",
     description="A Conversational AI platform.",
     long_description=readme,
     long_description_content_type="text/markdown",
