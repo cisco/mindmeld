f3c981cd0a4f2039d3d49237582b0d202c2e249e
Minh-Tue Vo <minhtue@cisco.com>
2020-10-12 16:11:03 -0700

    Bump version to 4.3.2rc4

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ test_requirements = [
 
 setup(
     name="mindmeld",
-    version="4.3.2rc3",
+    version="4.3.2rc4",
     description="A Conversational AI platform.",
     long_description=readme,
     long_description_content_type="text/markdown",
