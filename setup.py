ca14f312dba3821f3148942dc5b0c162b0823529
kunshar2 <kunshar2@cisco.com>
2020-11-19 12:26:03 -0800

    Specifying package version to resolve dependency issue.

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ with open("README.md") as readme_file:
     readme = readme_file.read()
 
 requirements = [
+   'dataclasses<0.8,>=0.7; python_version >= "3.6" and python_version < "3.7"',
     "aiohttp>=3.6.2",
     "attrs>=18.2",  # attrs has a stable API and does not use semver
     "Click~=7.1",
