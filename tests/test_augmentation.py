0b1cb443b486e582a5d7abbf7e4f6dff041ae04c
Ritvik Shrivastava <rs3868@columbia.edu>
2021-03-11 12:08:15 -0800

    fix test

--- tests/test_augmentation.py
+++ tests/test_augmentation.py
@@ -10,26 +10,36 @@ Tests the ``augmentation`` module.
 
 import pytest
 
-from mindmeld.augmentation import EnglishParaphraser
+from mindmeld.components._config import get_augmentation_config
+from mindmeld.models.helpers import create_augmentor
+from mindmeld.resource_loader import ResourceLoader
+
+NUM_PARAPHRASES = 10
 
 
 @pytest.fixture(scope="module")
 def english_paraphraser(kwik_e_mart_app_path):
-    return EnglishParaphraser(kwik_e_mart_app_path)
+    config = get_augmentation_config(app_path=kwik_e_mart_app_path)
+    config['params']['num_return_sequences'] = NUM_PARAPHRASES
+    lang = 'en'
+    resource_loader = ResourceLoader.create_resource_loader(kwik_e_mart_app_path)
+    augmentor = create_augmentor(
+        lang=lang, config=config, resource_loader=resource_loader
+    )
+    return augmentor
 
 
 @pytest.mark.extras
-@pytest.mark.augment
 @pytest.mark.parametrize(
-    "query, num_paraphrases",
+    "query",
     [
-        ("some text", 10),
-        ("another text", 5),
-        ("yet another text", 2),
+        ("some text"),
+        ("another text"),
+        ("yet another text"),
     ],
 )
-def test_num_paraphrases(english_paraphraser, query, num_paraphrases):
+def test_num_paraphrases(english_paraphraser, query):
     paraphrases = english_paraphraser.augment_queries(
-        [query], num_return_sequences=num_paraphrases
+        [query]
     )
-    assert num_paraphrases == len(paraphrases)
+    assert len(paraphrases) == 10
