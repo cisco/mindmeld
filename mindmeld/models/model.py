e1a265fa75664ab6f839f78539b12c6cbcb621a6
matt frey <mattfrey@cisco.com>
2021-05-19 18:55:22 -0400

    resource loading minor changee

--- mindmeld/models/model.py
+++ mindmeld/models/model.py
@@ -1195,9 +1195,8 @@ class Model:
                 self._resources[rname] = resource_loader.get_gazetteers()
             elif rname == SENTIMENT_ANALYZER:
                 self._resources[rname] = resource_loader.get_sentiment_analyzer()
-            if rname == SYS_TYPES_RSC:
+            elif rname == SYS_TYPES_RSC:
                 self._resources[rname] = resource_loader.get_sys_entity_types(labels)
-            # These resources require examples to build
             if examples:
                 if rname == WORD_FREQ_RSC:
                     resource_builders[rname] = resource_loader.WordFreqBuilder()
