6aa1d2be745bab2f974b0d977c58458c44fdbc35
Vijay Ramakrishnan <vijayrk@cisco.com>
2021-05-20 14:04:04 -0700

    Bump version: 4.3.5rc2 → 4.3.5rc3

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ test_requirements = [
 
 setup(
     name="mindmeld",
-    version="4.3.5rc2",
+    version="4.3.5rc3",
     description="A Conversational AI platform.",
     long_description=readme,
     long_description_content_type="text/markdown",
