add11251917b7c4b08322f71f6de41829f9d9e56
kunshar2 <kunshar2@cisco.com>
2021-07-29 14:46:09 -0700

    Added tests for the text_preparation_pipeline.py, merged regex tests into test_normalizers.

--- tests/text_preparation/test_normalizers.py
+++ tests/text_preparation/test_normalizers.py
@@ -8,7 +8,7 @@ test Normalizers
 Tests for Normalizers in the `text_preparation.normalizers` module.
 """
 import pytest
-
+from mindmeld.text_preparation.normalizers import DEFAULT_REGEX_NORM_RULES
 from mindmeld.text_preparation.normalizers import (
     ASCIIFold,
     NFD,
@@ -155,6 +155,232 @@ def test_get_regex_normalizers():
     regex_norm_rule = { "pattern": ".*", "replacement": ""}
     regex_normalizer = NormalizerFactory.get_regex_normalizers([regex_norm_rule])[0]
     assert regex_normalizer.normalize("Cisco") == ""
+
+
+# TESTING NORMALIZER CLASSES
+
+def _check_match(text_preparation_pipeline, regex_norm_rule, input_text, expected_text):
+    text_preparation_pipeline.normalizers = [DEFAULT_REGEX_NORM_RULES[regex_norm_rule]]
+    normalized_text = text_preparation_pipeline.normalize(input_text)
+    assert normalized_text == expected_text
+
+
+@pytest.mark.parametrize(
+    "input_text, expected_text",
+    [
+        ("that's dennis' truck", "that's dennis truck"),
+        ("where's luciens' cat?", "where's luciens cat?"),
+        ("JAMES' CAR", "JAMES CAR")
+    ],
+)
+def test_remove_apos_at_end_of_possesive_form(
+    text_preparation_pipeline, input_text, expected_text
+):
+    _check_match(
+        text_preparation_pipeline,
+        "RemoveAposAtEndOfPossesiveForm",
+        input_text,
+        expected_text,
+    )
+
+
+@pytest.mark.parametrize(
+    "input_text, expected_text",
+    [
+        ("          MindMeld", "MindMeld"),
+        ("      ", ""),
+        ("      How are you?", "How are you?"),
+    ],
+)
+def test_remove_beginning_space(text_preparation_pipeline, input_text, expected_text):
+    _check_match(
+        text_preparation_pipeline, "RemoveBeginningSpace", input_text, expected_text
+    )
+
+
+@pytest.mark.parametrize(
+    "input_text, expected_text",
+    [
+        ("MindMeld           ", "MindMeld"),
+        ("      ", ""),
+        ("How are you?     ", "How are you?"),
+    ],
+)
+def test_remove_trailing_space(text_preparation_pipeline, input_text, expected_text):
+    _check_match(
+        text_preparation_pipeline, "RemoveTrailingSpace", input_text, expected_text
+    )
+
+
+@pytest.mark.parametrize(
+    "input_text, expected_text",
+    [("How    are    you?", "How are you?"), ("I          am   fine!", "I am fine!")],
+)
+def test_replace_spaces_with_space(
+    text_preparation_pipeline, input_text, expected_text
+):
+    _check_match(
+        text_preparation_pipeline, "ReplaceSpacesWithSpace", input_text, expected_text
+    )
+
+
+@pytest.mark.parametrize(
+    "input_text, expected_text",
+    [("How_are_you?", "How are you?"), ("I_am_fine", "I am fine")],
+)
+def test_replace_underscore_with_space(
+    text_preparation_pipeline, input_text, expected_text
+):
+    _check_match(
+        text_preparation_pipeline,
+        "ReplaceUnderscoreWithSpace",
+        input_text,
+        expected_text,
+    )
+
+
+@pytest.mark.parametrize(
+    "input_text, expected_text",
+    [
+        ("mindmeld's code", "mindmeld 's code"),
+        ("k's", "k 's"),
+        ("CARL'S PIZZA", "CARL 's PIZZA"),
+    ],
+)
+def test_separate_apos_s(text_preparation_pipeline, input_text, expected_text):
+    _check_match(text_preparation_pipeline, "SeparateAposS", input_text, expected_text)
+
+
+@pytest.mark.parametrize(
+    "input_text, expected_text",
+    [
+        ("****mindmeld", "mindmeld"),
+        ("%#++=-=CISCO", "CISCO"),
+        ("///// //// //// NLP", "NLP"),
+    ],
+)
+def test_replace_punctuation_at_word_start_with_space(
+    text_preparation_pipeline, input_text, expected_text
+):
+    _check_match(
+        text_preparation_pipeline,
+        "ReplacePunctuationAtWordStartWithSpace",
+        input_text,
+        expected_text,
+    )
+
+
+@pytest.mark.parametrize(
+    "input_text, expected_text",
+    [
+        ("MindMeld*** is** the** best", "MindMeld is the best"),
+        ("How%+=* are++- you^^%", "How are you"),
+        ("I have $500 in my pocket!!!", "I have $500 in my pocket"),
+    ],
+)
+def test_replace_punctuation_at_word_end_with_space(
+    text_preparation_pipeline, input_text, expected_text
+):
+    _check_match(
+        text_preparation_pipeline,
+        "ReplacePunctuationAtWordEndWithSpace",
+        input_text,
+        expected_text,
+    )
+
+
+@pytest.mark.parametrize(
+    "input_text, expected_text",
+    [
+        ("Lucien has//++=/1 cat", "Lucien has 1 cat"),
+        ("Why!!!7 days?", "Why 7 days?"),
+        ("Racing^^!#%%24 hours##%$7 days", "Racing 24 hours 7 days"),
+    ],
+)
+def test_replace_special_chars_between_letters_and_digits_with_space(
+    text_preparation_pipeline, input_text, expected_text
+):
+    _check_match(
+        text_preparation_pipeline,
+        "ReplaceSpecialCharsBetweenLettersAndDigitsWithSpace",
+        input_text,
+        expected_text,
+    )
+
+
+@pytest.mark.parametrize(
+    "input_text, expected_text",
+    [
+        ("Lucien has 1//++=/cat", "Lucien has 1 cat"),
+        ("Why 7!!!days?", "Why 7 days?"),
+        ("Racing 24^^!#%%hours 7##%$days", "Racing 24 hours 7 days"),
+    ],
+)
+def test_replace_special_chars_between_digits_and_letters_with_space(
+    text_preparation_pipeline, input_text, expected_text
+):
+    _check_match(
+        text_preparation_pipeline,
+        "ReplaceSpecialCharsBetweenDigitsAndLettersWithSpace",
+        input_text,
+        expected_text,
+    )
+
+
+@pytest.mark.parametrize(
+    "input_text, expected_text",
+    [
+        ("Lucien has one//++=/cat", "Lucien has one cat"),
+        ("Why seven!!!days?", "Why seven days?"),
+        ("Racing all^^!#%%hours seven##%$days", "Racing all hours seven days"),
+    ],
+)
+def test_replace_special_chars_between_letters_with_space(
+    text_preparation_pipeline, input_text, expected_text
+):
+    _check_match(
+        text_preparation_pipeline,
+        "ReplaceSpecialCharsBetweenLettersWithSpace",
+        input_text,
+        expected_text,
+    )
+
+
+@pytest.mark.parametrize(
+    "input_text, expected_text",
+    [
+        ("Lucien has one//++=/|sys_number cat", "Lucien has one|sys_number cat"),
+        ("Why seven!!!|sys_number", "Why seven|sys_number"),
+        ("Racing all^^!#%%|custom_entity hours", "Racing all|custom_entity hours"),
+    ],
+)
+def test_replace_special_chars_before_pipe(
+    text_preparation_pipeline, input_text, expected_text
+):
+    _check_match(
+        text_preparation_pipeline,
+        "RemoveSpecialCharsBeforePipe",
+        input_text,
+        expected_text,
+    )
+
+
+@pytest.mark.parametrize(
+    "input_text, expected_text",
+    [
+        ("Lucien}+=-+s cat", "Lucien s cat"),
+        ("John]+=-+s dog", "John s dog"),
+    ],
+)
+def test_replace_end_bracket_and_following_special_chars_before_s_with_space(
+    text_preparation_pipeline, input_text, expected_text
+):
+    _check_match(
+        text_preparation_pipeline,
+        "ReplaceEndBracketAndFollowingSpecialCharsBeforeSWithSpace",
+        input_text,
+        expected_text,
+    )
 
 
 # TESTING NORMALIZATION ON SAMPLE SENTENCES
---
+++ tests/text_preparation/test_text_preparation_pipeline.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test TextPreparationPipeline
+----------------------------------
+
+Tests for TextPreparationPipeline in the `text_preparation.text_preparation_pipeline` module.
+"""
+import pytest
+
+from mindmeld.text_preparation.text_preparation_pipeline import TextPreparationPipeline, TextPreparationPipelineError
+from mindmeld.text_preparation.normalizers import Lowercase
+from mindmeld.text_preparation.tokenizers import WhiteSpaceTokenizer
+
+def test_text_preparation_pipeline_tokenizer_not_none():
+    with pytest.raises(TextPreparationPipelineError):
+        TextPreparationPipeline(tokenizer=None)
+
+
+def test_offset_token_start_values():
+    sample_token = {"start": 3}
+    TextPreparationPipeline.offset_token_start_values(tokens=[sample_token], offset=5)
+    assert sample_token["start"] == 8
+
+def test_filter_out_space_text_tokens():
+
+    input_tokens = [
+        {"text": "How"}, {"text": "   "}, {"text": "are"}, {"text": " "}, {"text": "you"},
+        {"text": "?"}
+    ]
+    expected_output_tokens = [
+        {'text': 'How'}, {'text': 'are'}, {'text': 'you'}, {'text': '?'}
+    ]
+    output_tokens = TextPreparationPipeline.filter_out_space_text_tokens(input_tokens)
+    assert expected_output_tokens == output_tokens
+
+def test_find_mindmeld_annotation_re_matches():
+
+    sentence = "Hello {Lucien|sys_person|employee}. Do you have {1|sys_number} cat?"
+    matches = TextPreparationPipeline.find_mindmeld_annotation_re_matches(sentence)
+    assert len(matches) == 2
+    first_match, second_match = matches
+    assert first_match.group(1) == "Lucien"
+    assert first_match.span() == (6, 34)
+    assert second_match.group(1) == "1"
+    assert second_match.span() == (48, 62)
+
+
+def test_normalize_around_annoations():
+
+    sentence = "HELLO {LUCIEN|PERSON_NAME}, HOW ARE YOU?"
+    normalized_sentence = TextPreparationPipeline.modify_around_annotations(
+        text=sentence, function=Lowercase().normalize
+    )
+    expected_normalized_sentence = 'hello {lucien|PERSON_NAME}, how are you?'
+    assert normalized_sentence == expected_normalized_sentence
+
+
+def test_tokenize_around_annoations():
+
+    sentence = "HELLO {LUCIEN|PERSON_NAME}, HOW ARE YOU?"
+    raw_tokens = TextPreparationPipeline.tokenize_around_mindmeld_annotations(
+        text=sentence, function=WhiteSpaceTokenizer().tokenize
+    )
+    expected_raw_tokens = [
+        {'start': 0, 'text': 'HELLO'},
+        {'start': 7, 'text': 'LUCIEN'},
+        {'start': 26, 'text': ','},
+        {'start': 28, 'text': 'HOW'},
+        {'start': 32, 'text': 'ARE'},
+        {'start': 36, 'text': 'YOU?'}
+    ]
+    assert raw_tokens == expected_raw_tokens
+
+
