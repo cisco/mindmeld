b481b19fb8cf4c267b5e489da79076f67882ab0f
Vinay Damodaran <vrdn@hey.com>
2021-09-24 14:51:15 -0700

    Bump version: 4.3.5rc11 → 4.3.5rc12

--- mindmeld/_version.py
+++ mindmeld/_version.py
@@ -14,7 +14,7 @@
 """Defines mindmeld version information"""
 from __future__ import absolute_import, unicode_literals
 
-current = "4.3.5rc11"
+current = "current = '4.3.5rc12'"
 
 
 def get_mm_version():
--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ test_requirements = [
 
 setup(
     name="mindmeld",
-    version="4.3.5rc11",
+    version="version='4.3.5rc12'",
     description="A Conversational AI platform.",
     long_description=readme,
     long_description_content_type="text/markdown",
