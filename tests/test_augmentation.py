49585b517f1065e7e4d36f907e98d1c14dac5bdd
vembar <vembar@cisco.com>
2021-09-30 16:32:45 -0700

    change order to test memory

--- tests/test_augmentation.py
+++ tests/test_augmentation.py
@@ -19,8 +19,48 @@ NUM_PARAPHRASES = 10
 
 
 @pytest.fixture(scope="module")
+def english_paraphraser_retain_entities(kwik_e_mart_app_path):
+    config = get_augmentation_config(app_path=kwik_e_mart_app_path)
+    language = "en"
+    config['augmentor_class'] = "EnglishParaphraser"
+    config['retain_entities'] = True
+    query_factory = QueryFactory.create_query_factory(app_path=kwik_e_mart_app_path, duckling=True)
+    resource_loader = ResourceLoader.create_resource_loader(app_path=kwik_e_mart_app_path,
+                                                            query_factory=query_factory)
+    augmentor = AugmentorFactory(
+        config=config,
+        language=language,
+        resource_loader=resource_loader,
+    ).create_augmentor()
+    return augmentor
+
+
+@pytest.mark.extras
+@pytest.mark.parametrize(
+    "query, entity_types",
+    [
+        ("some text that contains no entities", [],),
+        (
+            "can you tell me if {springfield|store_name} is possibly open at this time on {friday|sys_time}",
+            ['store_name', 'sys_time'],
+        ),
+        (
+            "Open the {china town|store_name} at {1 pm|sys_time|opening_time}",
+            ['store_name', 'sys_time|opening_time'],
+        ),
+    ],
+)
+def test_paraphrases_with_entities(english_paraphraser_retain_entities, query, entity_types):
+    paraphrases = english_paraphraser_retain_entities.augment_queries([query])
+    for p in paraphrases:
+        for entity in entity_types:
+            assert entity in p
+
+
+@pytest.fixture(scope="module")
 def english_paraphraser(kwik_e_mart_app_path):
     config = get_augmentation_config(app_path=kwik_e_mart_app_path)
+    config['retain_entities'] = False
     language = "en"
     resource_loader = ResourceLoader.create_resource_loader(kwik_e_mart_app_path)
     augmentor = AugmentorFactory(
@@ -79,42 +119,3 @@ def test_spanish_paraphrases(kwik_e_mart_app_path, query):
 
     paraphrases = multilingual_paraphraser.augment_queries([query])
     assert "aumentar el volumen" in paraphrases
-
-
-@pytest.fixture(scope="module")
-def english_paraphraser_retain_entities(kwik_e_mart_app_path):
-    config = get_augmentation_config(app_path=kwik_e_mart_app_path)
-    language = "en"
-    config['augmentor_class'] = "EnglishParaphraser"
-    config['retain_entities'] = True
-    query_factory = QueryFactory.create_query_factory(app_path=kwik_e_mart_app_path, duckling=True)
-    resource_loader = ResourceLoader.create_resource_loader(app_path=kwik_e_mart_app_path,
-                                                            query_factory=query_factory)
-    augmentor = AugmentorFactory(
-        config=config,
-        language=language,
-        resource_loader=resource_loader,
-    ).create_augmentor()
-    return augmentor
-
-
-@pytest.mark.extras
-@pytest.mark.parametrize(
-    "query, entity_types",
-    [
-        ("some text that contains no entities", [],),
-        (
-            "can you tell me if {springfield|store_name} is possibly open at this time on {friday|sys_time}",
-            ['store_name', 'sys_time'],
-        ),
-        (
-            "Open the {china town|store_name} at {1 pm|sys_time|opening_time}",
-            ['store_name', 'sys_time|opening_time'],
-        ),
-    ],
-)
-def test_paraphrases_with_entities(english_paraphraser_retain_entities, query, entity_types):
-    paraphrases = english_paraphraser_retain_entities.augment_queries([query])
-    for p in paraphrases:
-        for entity in entity_types:
-            assert entity in p
