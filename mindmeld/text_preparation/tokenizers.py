53d280ccfe20b0a6e5aef27cf224f05d049c5169
kunshar2 <kunshar2@cisco.com>
2022-03-02 15:05:22 -0600

    Expanding the disable list.

--- mindmeld/text_preparation/tokenizers.py
+++ mindmeld/text_preparation/tokenizers.py
@@ -241,7 +241,7 @@ class SpacyTokenizer(Tokenizer):
             spacy_model_size (str, optional): Size of the Spacy model to use. ("sm", "md", or "lg")
         """
         self.spacy_model = SpacyModelFactory.get_spacy_language_model(
-            language, spacy_model_size, disable=["tagger", "parser", "ner"]
+            language, spacy_model_size, disable=["tagger", "parser", "ner", "attribute_ruler", "lemmatizer"]
         )
         assert self.spacy_model.pipeline == []
 
